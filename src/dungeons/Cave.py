8a9a44e41ee893cfae9062d37b98da9c08488be7
Hramov2 <hramovpi@gmail.com>
2025-11-22 22:05:07 +0800

    Added sanctions for border useless space

--- src/dungeons/Cave.py
+++ src/dungeons/Cave.py
@@ -1,5 +1,5 @@
 import copy
-from src.services.arithmetic_service import count_dist
+from src.services.arithmetic_service import count_dist, count_sum_around, count_space, iterations
 from src.dungeons.Dungeon import Dungeon
 
 
@@ -47,7 +47,25 @@ class Cave(Dungeon):
                     if 1 < i < self.n - 2 and 1 < j < self.m - 2:
                         if state[i - 1][j] + state[i + 1][j] + state[i][j - 1] + state[i][j + 1] >= 3:
                             if state[i - 2][j] + state[i + 2][j] + state[i][j - 2] + state[i][j + 2] <= 2:
-                                score += 2
+                                score += 2.3
+        return score
+
+    def decrease_useless_border_space(self, state, entry):
+        score = 0
+        for i in range(len(entry)):
+            if count_sum_around(entry[i][0], entry[i][1], state) == 3:
+                score -= 3
+        return score
+
+    def decrease_too_many_entries(self, entry):
+        return 10 if len(entry) <= 3 else -2 * (len(entry) - 2)
+
+    def decrease_group_useless_border_space(self, state, entry):
+        score = 0
+        for i in range(len(entry)):
+            count_space(entry[i][0], entry[i][1], copy.deepcopy(state))
+            if iterations <= 5:
+                score -= 3
         return score
 
     def infection_cave(self, i, j, state):
@@ -81,5 +99,8 @@ class Cave(Dungeon):
         score = self.score
         self.score = 0
         score += self.increase_midwalls(state)
+        score += self.decrease_too_many_entries(entry)
+        #score += self.decrease_useless_border_space(state, entry)
+        score += self.decrease_group_useless_border_space(state, entry)
 
         return score
--- src/services/arithmetic_service.py
+++ src/services/arithmetic_service.py
@@ -31,3 +31,55 @@ def count_dist(i, j, state, n, m):
     right = max(s - j - 1, 0)
 
     return left, up, right, down
+
+
+def count_sum_around(i, j, state):
+    su = 0
+    try:
+        su += state[i - 1][j]
+    except:
+        pass
+
+    try:
+        su += state[i + 1][j]
+    except:
+        pass
+
+    try:
+        su += state[i][j + 1]
+    except:
+        pass
+
+    try:
+        su += state[i][j - 1]
+    except:
+        pass
+
+    return su
+
+
+iterations = 0
+
+
+def count_space(i, j, state, first=True):
+    global iterations
+    if first:
+        iterations = 0
+
+    iterations += 1
+    n = len(state)
+    m = len(state[0])
+
+    state[i][j] = "*"
+
+    if i - 1 >= 0 and state[i - 1][j] == 0:
+        count_space(i - 1, j, state, False)
+
+    if i + 1 < n and state[i + 1][j] == 0:
+        count_space(i + 1, j, state, False)
+
+    if j - 1 >= 0 and state[i][j - 1] == 0:
+        count_space(i, j - 1, state, False)
+
+    if j + 1 < m and state[i][j + 1] == 0:
+        count_space(i, j + 1, state, False)
